38b03d1696d64aa8a625aa7f4f7862225633c471
Orlando Lopez <orlandolopez@Orlandos-MBP.fios-router.home>
2018-12-05 19:07:16 -0500

    added article submittion logic

--- Article/Article.js
+++ Article/Article.js
@@ -1,20 +1,30 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
+const articlesElement = document.querySelector('.articles');
+const maker = () =>  {
+let articles = document.querySelectorAll('.article');
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "Click to Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', () => this.expandArticle(this.expandButton))
+    // STRETCH CLOSE BUTTON
+    this.closeButton = this.domElement.querySelector('.closeBtn');
+    this.closeButton.addEventListener('click', () => this.deleteArticle(this.domElement));
   }
 
-  expandArticle() {
+  expandArticle(btn) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
+    btn.textContent === "Click to Close" ? btn.textContent = "Click to Expand" : btn.textContent = "Click to Close";
+  }
+  deleteArticle(article){
+    // article.style.display = "none";
+    articlesElement.removeChild(article)
   }
 }
 
@@ -25,5 +35,38 @@ class Article {
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
 
 */
+articles.forEach((article) => new Article(article));
+}
+maker()
 
-let articles;
+//STRETCH ADD ARTICLE
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const form = document.querySelector('form');
+
+form.addEventListener('submit' , (e) => {
+  e.preventDefault();
+  console.log(e.target.title.value)
+  let wrappingDiv = document.createElement('div')
+  wrappingDiv.classList.add('article');
+  let headerTwo = document.createElement('h2');
+  let dateParagraph = document.createElement('p');
+  const date = new Date();
+  dateParagraph.textContent =  `${months[date.getUTCMonth()]} ${date.getUTCDate()}, ${date.getUTCFullYear()}`;
+  dateParagraph.classList.add('date')
+  let paragraph = document.createElement('p');
+  let span = document.createElement('span');
+  let button = document.createElement('button');
+  button.textContent = "X";
+  button.classList.add('closeBtn');
+  button.type = "button";
+  span.classList.add('expandButton');
+  headerTwo.textContent = e.target.title.value;
+  paragraph.textContent = e.target.title.value;
+  wrappingDiv.appendChild(headerTwo);
+  wrappingDiv.appendChild(dateParagraph)
+  wrappingDiv.appendChild(paragraph);
+  wrappingDiv.appendChild(span);
+  wrappingDiv.appendChild(button);
+  articlesElement.appendChild(wrappingDiv);
+  maker()
+})
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,18 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  backdrop.classList.toggle('show');
+  menu.classList.toggle('menu--open')
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', toggleMenu)
+
+
+const backdrop = document.querySelector('.backdrop');
+
+backdrop.addEventListener('click', () => toggleMenu())
